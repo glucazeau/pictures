bcc974196a325e5b80165b92a2cba7f49c5331d2
Guillaume Lucazeau <glucazeau@advanced-schema.com>
2020-05-01 18:21:37 +0200

    Allow recursive file listing

--- common.py
+++ common.py
@@ -5,6 +5,5 @@ def list_files(source_path):
     extensions = ["png", "jpg", "jpeg", "gif"]
     files = []
     for extension in extensions:
-        files += (glob.glob(f"{source_path}/*.{extension}"))
-    #files = [f for f in listdir(source_path) if isfile(join(source_path, f))]
+        files += (glob.glob(f"{source_path}/**/*.{extension}", recursive=True))
     return files
